ae7c6426e85d38abb926b805c90e8d47b5ffeaa9
Tony Vazgar <tonyvazgar@Tonys-MacBook-Pro.local>
2021-03-04 17:53:39 -0600

    Bug de nombre de usuario corregido

--- reportes.php
+++ reportes.php
@@ -4,6 +4,12 @@ $email = $_SESSION['email'];
 $password = $_SESSION['password'];
 if($email == false && $password == false){
   header('Location: login.php');
+}else{
+  $sql = "SELECT * FROM usertable WHERE email = '$email'";
+  $run_Sql = mysqli_query($con, $sql);
+  if($run_Sql){
+    $fetch_info = mysqli_fetch_assoc($run_Sql);
+  }
 }
 ?>
 <!DOCTYPE html>
