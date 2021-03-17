737582017a1d88414fef5c8740463b92f1324e9b
Tony <>
2021-03-17 00:37:20 -0600

    US 3.2 Alta de cliente

---
+++ prosking_gestor.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 17, 2021 at 07:36 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prosking_gestor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Cliente`
+--
+
+CREATE TABLE `Cliente` (
+  `id_cliente` varchar(255) COLLATE utf8_bin NOT NULL,
+  `nombre_cliente` varchar(255) COLLATE utf8_bin NOT NULL,
+  `apellidos_cliente` varchar(255) COLLATE utf8_bin NOT NULL,
+  `telefono_cliente` varchar(10) COLLATE utf8_bin NOT NULL,
+  `email_cliente` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `Cliente`
+--
+
+INSERT INTO `Cliente` (`id_cliente`, `nombre_cliente`, `apellidos_cliente`, `telefono_cliente`, `email_cliente`) VALUES
+('LVG9611071', 'Luis Antonio', 'Vazquez Garcia', '2226772173', 'tony@tony.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ClienteOpcional`
+--
+
+CREATE TABLE `ClienteOpcional` (
+  `id_cliente` varchar(255) COLLATE utf8_bin NOT NULL,
+  `fecha_cliente` date NOT NULL,
+  `cp_cliente` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `ClienteOpcional`
+--
+
+INSERT INTO `ClienteOpcional` (`id_cliente`, `fecha_cliente`, `cp_cliente`) VALUES
+('LVG9611071', '1996-11-07', '72830');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertable`
+--
+
+CREATE TABLE `usertable` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `code` mediumint(50) NOT NULL,
+  `status` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `usertable`
+--
+
+INSERT INTO `usertable` (`id`, `name`, `email`, `password`, `code`, `status`) VALUES
+(8, 'Tony', 'tony@tony.com', '$2y$10$XfrGDv443rXAgesAhSfiTu8AAJWolQDxteAQ/ZxOG7pUP8qTPTS1C', 635339, 'notverified');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Cliente`
+--
+ALTER TABLE `Cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indexes for table `ClienteOpcional`
+--
+ALTER TABLE `ClienteOpcional`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indexes for table `usertable`
+--
+ALTER TABLE `usertable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usertable`
+--
+ALTER TABLE `usertable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
